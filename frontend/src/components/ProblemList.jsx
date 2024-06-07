eeaa55fb38344157cbb58267de70be05886aa365
Siddh Manek <awesomesiddh@gmail.com>
2024-06-08 02:13:08 +0530

    added delete functionality but not working properly

--- frontend/src/components/ProblemList.jsx
+++ frontend/src/components/ProblemList.jsx
@@ -3,11 +3,13 @@ import { MdEdit } from "react-icons/md";
 import { FaPlay } from "react-icons/fa";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
-
+import { MdDelete } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
 function ProblemList() {
 
         const [problems,setProblems] = useState([])
+
+        const navigate = useNavigate()
 
         useEffect(()=>{
             const getProblems = async()=>{
@@ -24,6 +26,26 @@ function ProblemList() {
     },[])
 
 
+    const handleDelete = async(title)=>{
+        const answer = window.confirm('Are you sure you want to delete this problem it cannot be recovered later!')
+        try {
+            if(answer){
+                let deleteURL = `http://localhost:8000/api/v1/users/deleteProblem`
+                const response = await axios.delete(deleteURL,{title:title},{withCredentials:true})
+                if(response && response.status==200){
+                    alert('Problem deleted Successfully')
+                    Navigate('../user/ProblemList')
+                }
+                else{
+                    throw new Error()
+                }
+            }
+        } catch (error) {
+            alert('Error occured while deleting')
+        }
+    }
+
+
     return (
         < >
           <div className='border bg-slate-500 h-screen'>
@@ -33,8 +55,9 @@ function ProblemList() {
                     <div  key={problem.title} className='bg-customDark container mx-auto mt-9 w-2/3 text-white rounded-lg flex justify-between p-3 items-center'>
                         {problem.title}
                     <div className='flex justify-around  w-1/5'>
-                    <Link to={`../updateProblem/${problem.title}`}><MdEdit /></Link>
                     <Link to={`../submit/${problem.title}`}> <FaPlay onClick={()=>handleSubmit(problem.title)}/></Link>
+                    <Link to={`../updateProblem/${problem.title}`}><MdEdit /></Link>
+                    <button onClick={()=>handleDelete(problem.title)}><MdDelete /></button>
                     </div>
                 </div>
                 ))
